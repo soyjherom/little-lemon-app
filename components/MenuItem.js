0a8486b74ee22035e0564fd690edc690830c574e
soyjherom <jherom@tecnica.co.cr>
2023-01-31 14:55:34 -0600

    [MODIFY] MenuItem component to add onPress support to add elements from car or cancel the action

--- components/MenuItem.js
+++ components/MenuItem.js
@@ -5,23 +5,69 @@ import {
   TouchableHighlight, 
   View } from 'react-native'
 
-export const MenuItem = ({ item }) => {
+import { useCallback, useState } from 'react'
+
+export const MenuItem = ({ item }, key) => {
+  
+  const [selected, setSelected] = useState(false)
+  const [unselected, setUnselected] = useState(false)
+  
+  const toggleSelected = useCallback(()=>{
+    setSelected(!selected)
+    setUnselected(!unselected)
+  },[setSelected, selected, setUnselected, unselected])
+  
+  const addToCar = useCallback(()=>{
+    console.log(`Adding to car: ${item.name}`)
+    setSelected(false)
+    setUnselected(false)
+  },[setSelected, setUnselected])
+
+  const cancel = useCallback(()=>{
+    setUnselected(false)
+    setSelected(false)
+  },[setUnselected, setSelected])
+
   return (
-    <TouchableHighlight onPress = {
-      ()=>{
-        console.log('I has been tapped')
+    <View style={styles.itemContainer}>
+      {unselected && 
+        <TouchableHighlight onPress = {cancel}>
+          <View style={styles.unSelector}>
+            <Text style={{margin: 10}}>Cancel</Text>
+          </View>
+        </TouchableHighlight>
       }
-    }>
-      <View 
-        style={styles.itemContainer}>
-        <Text style={styles.item}>{item.name} </Text>
-        <Text style={styles.item}>{item.price}</Text>
-      </View>
-    </TouchableHighlight>
+      <TouchableHighlight style={styles.itemContainer}
+        onPress = {toggleSelected}>
+        <View style={styles.itemContainer}>
+          <Text style={styles.item}>{item.name} </Text>
+          <Text style={styles.item}>{item.price}</Text>
+        </View>
+      </TouchableHighlight>
+      {selected && 
+        <TouchableHighlight onPress = {addToCar}>
+          <View style={styles.selector}>
+            <Text style={{margin: 10}}>   Add </Text>
+          </View>
+        </TouchableHighlight>
+      }
+    </View>
   )
 }
 
 const styles = StyleSheet.create({
+  selector:{
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'green',
+  },
+  unSelector:{
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'red',
+  },
   item: {margin: 10},
   itemContainer: {
     backgroundColor: '#F4CE14',
