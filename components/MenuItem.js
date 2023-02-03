da283ffcf2a5cf7dace5be92b58d92c7c737963d
soyjherom <jherom@tecnica.co.cr>
2023-02-03 14:46:11 -0600

    [ADD] Dish Card

--- components/MenuItem.js
+++ components/MenuItem.js
@@ -1,5 +1,6 @@
 
 import { 
+  Image,
   StyleSheet, 
   Text,
   TouchableHighlight, 
@@ -7,66 +8,119 @@ import {
 
 import { useCallback, useState } from 'react'
 
+import NumericInput from 'react-native-numeric-input'
+
 export const MenuItem = ({ item }, key) => {
   
   const [selected, setSelected] = useState(false)
-  const [unselected, setUnselected] = useState(false)
+  const [quantity, setQuantity] = useState(1)
   
   const toggleSelected = useCallback(()=>{
     setSelected(!selected)
-    setUnselected(!unselected)
-  },[setSelected, selected, setUnselected, unselected])
+  },[setSelected, selected])
   
   const addToCar = useCallback(()=>{
     console.log(`Adding to car: ${item.name}`)
     setSelected(false)
-    setUnselected(false)
-  },[setSelected, setUnselected])
+  },[setSelected])
 
   const cancel = useCallback(()=>{
-    setUnselected(false)
     setSelected(false)
-  },[setUnselected, setSelected])
+    setQuantity(1)
+  },[setSelected, setQuantity])
+
+  const handleOnChangeQuantity = useCallback((input)=>{
+    setQuantity(input)
+  },[setQuantity])
 
   return (
-    <View style={styles.itemContainer}>
-      {unselected && 
-        <TouchableHighlight onPress = {cancel}>
-          <View style={styles.unSelector}>
-            <Text style={{margin: 10}}>Cancel</Text>
+    <>
+      <View style={styles.itemContainer}>
+        <TouchableHighlight style={styles.itemContainer}
+          onPress = {toggleSelected}>
+          <View style={styles.itemContainer}>
+            <Text style={styles.item}>{item.name} </Text>
+            <Text style={styles.item}>{item.price}</Text>
           </View>
         </TouchableHighlight>
-      }
-      <TouchableHighlight style={styles.itemContainer}
-        onPress = {toggleSelected}>
-        <View style={styles.itemContainer}>
-          <Text style={styles.item}>{item.name} </Text>
-          <Text style={styles.item}>{item.price}</Text>
-        </View>
-      </TouchableHighlight>
+      </View>
       {selected && 
-        <TouchableHighlight onPress = {addToCar}>
-          <View style={styles.selector}>
-            <Text style={{margin: 10}}>   Add </Text>
+      <View style={styles.dishCard}>
+        <Text style={styles.dishCardHeading}>Ingredients</Text>
+        <View style={styles.dishIngredients}>
+          <Image style={styles.logo} 
+            source={{uri:item.image}}/>
+          <View style={styles.dishIngredient}>
+            {item.ingredients?.map(i => 
+              <Text>{i}</Text>
+            )}
           </View>
-        </TouchableHighlight>
-      }
-    </View>
+        </View>
+        <View style={styles.itemContainer}>
+          <TouchableHighlight onPress = {cancel}>
+            <View style={styles.cancelButton}>
+              <Text style={{margin: 10}}>No thanks</Text>
+            </View>
+          </TouchableHighlight>
+          <NumericInput value={quantity}
+            onChange={handleOnChangeQuantity}
+            totalWidth={150}
+            minValue={1}/>
+          <TouchableHighlight onPress = {addToCar}>
+            <View style={styles.addButton}>
+              <Text style={{margin: 10}}>Yes I want this</Text>
+            </View>
+          </TouchableHighlight>
+        </View>
+      </View>}
+    </>
   )
 }
 
 const styles = StyleSheet.create({
-  selector:{
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: 'green',
+  dishCard: {
+    flex:1, 
+    justifyContent: 'space-around', 
+    alignItems: 'center', 
+    marginBottom: 5
   },
-  unSelector:{
+  dishCardHeading: {
+    color:'white', 
+    fontSize: 18, 
+    alignText: 'center', 
+    padding: 10
+  },
+  dishIngredients: {
+    flex: 1, 
+    flexDirection: 'row', 
+    justifyContent: 'space-between', 
+    backgroundColor: '#BDECB6', 
+    marginBottom: 5
+  },
+  dishIngredient: {
+    flex:1, 
+    alignItems: 'flex-start',
+    padding: 10
+  },
+  logo:{
+    width: 150,
+    height: 150,
+  },
+  addButton:{
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: 'red',
+    backgroundColor: '#5DBB63',
+    borderRadius: 5,
+    width: 120,
+  },
+  cancelButton:{
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F0967C',
+    borderRadius: 5,
+    width: 120,
   },
   item: {margin: 10, marginTop:20, marginBottom: 20},
   itemContainer: {
@@ -76,5 +130,5 @@ const styles = StyleSheet.create({
     justifyContent: 'space-between',
     alignItems: 'center',
     borderRadius: 5,
-  }
+  },
 })
--- components/MenuSectionList.js
+++ components/MenuSectionList.js
@@ -10,39 +10,39 @@ const menuElements = [
   {
     title: 'Appetizers',
     data: [
-      {name: 'Humus',price: '$5', id: uuid.v4()},
-      {name: 'Moutabal',price: '$7',  id: uuid.v4()}, 
-      {name: 'Falafel',price: '$5', id: uuid.v4()},
-      {name: 'Marinated Olives',price: '$3', id: uuid.v4()},
-      {name: 'Kofta',price: '$4', id: uuid.v4()},
-      {name: 'Eggplant Salad',price: '$10', id: uuid.v4()},
+      {name: 'Humus',price: '$5', id: uuid.v4(), ingredients: ['potato', 'tomato', 'tobaco', 'olive oil'], image:'https://www.simplyrecipes.com/thmb/J-X1xpDU2606pUXnmL4a7409sHk=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/__opt__aboutcom__coeus__resources__content_migration__simply_recipes__uploads__2017__12__easy-hummus-vertical-a-1800-2ee0f05419d744bcb85017bce019c7bf.jpg'},
+      {name: 'Moutabal',price: '$7',  id: uuid.v4(), ingredients: ['potato', 'tomato', 'tobaco', 'olive oil'],image:'https://picsum.photos/200'}, 
+      {name: 'Falafel',price: '$5', id: uuid.v4(), ingredients: ['potato', 'tomato', 'tobaco', 'olive oil'],image:'https://picsum.photos/200'},
+      {name: 'Marinated Olives',price: '$3', id: uuid.v4(), ingredients: ['potato', 'tomato', 'tobaco', 'olive oil'],image:'https://picsum.photos/200'},
+      {name: 'Kofta',price: '$4', id: uuid.v4(), ingredients: ['potato', 'tomato', 'tobaco', 'olive oil'],image:'https://picsum.photos/200'},
+      {name: 'Eggplant Salad',price: '$10', id: uuid.v4(), ingredients: ['potato', 'tomato', 'tobaco', 'olive oil'],image:'https://picsum.photos/200'},
     ]
   },
   {
     title:'Main dishes',
     data:[
-      {name: 'Lentil Burger',price: '$15', id: uuid.v4()},
-      {name: 'Smoked Salmon',price: '$25', id: uuid.v4()},
-      {name: 'Kafta Burger',price: '$14', id: uuid.v4()},
-      {name: 'Turkish Kebab',price: '$15', id: uuid.v4()},
+      {name: 'Lentil Burger',price: '$15', id: uuid.v4(), ingredients: ['potato', 'tomato', 'tobaco', 'olive oil'],image:'https://picsum.photos/200'},
+      {name: 'Smoked Salmon',price: '$25', id: uuid.v4(), ingredients: ['potato', 'tomato', 'tobaco', 'olive oil'],image:'https://picsum.photos/200'},
+      {name: 'Kafta Burger',price: '$14', id: uuid.v4(), ingredients: ['potato', 'tomato', 'tobaco', 'olive oil'],image:'https://picsum.photos/200'},
+      {name: 'Turkish Kebab',price: '$15', id: uuid.v4(), ingredients: ['potato', 'tomato', 'tobaco', 'olive oil'],image:'https://picsum.photos/200'},
     ]
   },
   {
     title: 'Sides',
     data: [
       {name: 'Fries',price: '$5', id: uuid.v4()},
-      {name: 'Buttered Rice',price: '$7', id: uuid.v4()},
-      {name: 'Bread sticks',price: '$3', id: uuid.v4()},
-      {name: 'Pita Pocket',price: '$3', id: uuid.v4()},
-      {name: 'Lentil Soup',price: '$8', id: uuid.v4()},
-      {name: 'Greek Salad',price: '$10', id: uuid.v4()},
-      {name: 'Rice Pilaf',price: '$9', id: uuid.v4()},
+      {name: 'Buttered Rice',price: '$7', id: uuid.v4(), ingredients: ['potato', 'tomato', 'tobaco', 'olive oil'],image:'https://picsum.photos/200'},
+      {name: 'Bread sticks',price: '$3', id: uuid.v4(), ingredients: ['potato', 'tomato', 'tobaco', 'olive oil'],image:'https://picsum.photos/200'},
+      {name: 'Pita Pocket',price: '$3', id: uuid.v4(), ingredients: ['potato', 'tomato', 'tobaco', 'olive oil'],image:'https://picsum.photos/200'},
+      {name: 'Lentil Soup',price: '$8', id: uuid.v4(), ingredients: ['potato', 'tomato', 'tobaco', 'olive oil'],image:'https://picsum.photos/200'},
+      {name: 'Greek Salad',price: '$10', id: uuid.v4(), ingredients: ['potato', 'tomato', 'tobaco', 'olive oil'],image:'https://picsum.photos/200'},
+      {name: 'Rice Pilaf',price: '$9', id: uuid.v4(), ingredients: ['potato', 'tomato', 'tobaco', 'olive oil'],image:'https://picsum.photos/200'},
     ]
   },
   {
     title: 'Desserts',
     data: [
-      {name: 'Baklava',price: '$3', id: uuid.v4()},
+      {name: 'Baklava',price: '$3', id: uuid.v4(), ingredients: ['potato', 'tomato', 'tobaco', 'olive oil'],image:'https://picsum.photos/200'},
     ]
   }
 ]
